685843bd43a8c49a6816b78674b8e8e9d9d4b2b4
sheebanaraza <shaikhsheebanz@gmail.com>
2015-09-25 18:25:25 -0700

    added controller for charts

--- src/controller/CampusConnectController.java
+++ src/controller/CampusConnectController.java
@@ -3,10 +3,15 @@ package controller;
 import model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView;
 import service.CampusConnectService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 /**
  * Created by Sheeban
@@ -18,10 +23,17 @@ public class CampusConnectController {
     @Autowired
     private CampusConnectService campusConnectService;
 
-    @RequestMapping(value = "/authenticateUser/CampusConnect.htm")
-    public ModelAndView authenticateUser(@RequestParam("user") String twitterUserName) throws Exception {
-        ModelAndView modelAndView = new ModelAndView("/index.jsp");
-        User user = campusConnectService.getUserDetails(twitterUserName);
+    @RequestMapping(value = "/authenticateUser.htm")
+    public ModelAndView authenticateUser(@ModelAttribute(value = "user") User user,
+                  HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                    @RequestParam("user") String twitterUserName) throws Exception {
+
+        ModelAndView modelAndView = new ModelAndView("/charts");
+        Boolean userExists = campusConnectService.getUserDetails("sheebanshaikh");
+        modelAndView.addObject("user", user);
+        Map<String,String> userDetails = campusConnectService.getNameVsUserNameMap("sheebanshaikh");
+
+        modelAndView.addObject("usersDataMap",userDetails);
         return modelAndView;
     }
 
